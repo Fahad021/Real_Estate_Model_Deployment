2a72e6c78a4cb41ff860387d9ae721551267a19a
Sourcery AI <>
2023-08-05 21:30:56 +0000

    'Refactored by Sourcery'

--- app.py
+++ app.py
@@ -11,19 +11,21 @@ def home():
 	
 @app.route('/predict',methods=["POST","GET"])
 def predict():
-  a=[]        
-  if request.method == "POST":
-    dtd = request.form['dtd']
-    age = request.form['age']
-    station = request.form['station']
-    store = request.form['store']
-    lat = request.form['lat']
-    log = request.form['log']
-    a.append([dtd,age,station,store,lat,log])
-    final_features = np.asarray(a)
-    prediction = model.predict(final_features)
-    output = round(prediction[0], 2)
-  return render_template('home.html', prediction_text='HOUSE PRICE PER UNIT AREA IS {}'.format(output))
+	a=[]
+	if request.method == "POST":
+	  dtd = request.form['dtd']
+	  age = request.form['age']
+	  station = request.form['station']
+	  store = request.form['store']
+	  lat = request.form['lat']
+	  log = request.form['log']
+	  a.append([dtd,age,station,store,lat,log])
+	  final_features = np.asarray(a)
+	  prediction = model.predict(final_features)
+	  output = round(prediction[0], 2)
+	return render_template(
+		'home.html', prediction_text=f'HOUSE PRICE PER UNIT AREA IS {output}'
+	)
 
 if __name__ == "__main__":
     app.run(debug=True)
